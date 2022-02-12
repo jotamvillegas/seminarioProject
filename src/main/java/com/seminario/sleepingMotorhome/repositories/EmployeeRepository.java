f2f4d951b21e2718e5800a2761e2f4b97c868922
jotamvillegas <Jotamvillegas86@gmail.com>
2022-02-12 19:02:38 -0300

    crud services employee

--- src/main/java/com/seminario/sleepingMotorhome/repositories/EmployeeRepository.java
+++ src/main/java/com/seminario/sleepingMotorhome/repositories/EmployeeRepository.java
@@ -1,11 +1,9 @@
 package com.seminario.sleepingMotorhome.repositories;
 
-import com.seminario.sleepingMotorhome.models.Person;
+import com.seminario.sleepingMotorhome.models.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository <Person, Long> {
-
-
+public interface EmployeeRepository extends CrudRepository <Employee, Long> {
 }
