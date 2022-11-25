448a99bb52ea4455485f31abd9413c5685498e27
jotamvillegas <Jotamvillegas86@gmail.com>
2022-11-24 21:09:58 -0300

    fix delete services

--- src/main/java/com/seminario/sleepingMotorhome/repositories/ServiceRepository.java
+++ src/main/java/com/seminario/sleepingMotorhome/repositories/ServiceRepository.java
@@ -1,11 +1,16 @@
 package com.seminario.sleepingMotorhome.repositories;
 
 import com.seminario.sleepingMotorhome.models.Service;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceRepository extends CrudRepository <Service, Long> {
 
-    boolean existsServiceByDescription (String service);
+    @Query(value = "SELECT COUNT(t.id) FROM task t, service s WHERE t.service_id = s.id AND t.service_id = ?1", nativeQuery = true)
+    int numberOfTask (Long servideId);
+
+    @Query(value = "SELECT SUM(t.is_active) FROM task t, service s WHERE t.service_id = s.id AND t.service_id = ?1", nativeQuery = true)
+    int getTasksInFinishedState (Long servideId);
 }
